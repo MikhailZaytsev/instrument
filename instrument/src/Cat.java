d7369fabbdb91bec34ab3d00629964601b29c200
Михаил <mikhailzaytsev02.1996@gmail.com>
2022-04-13 15:53:12 +0300

    Cat added

---
+++ instrument/src/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    String name;
+    int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void meow() {
+        System.out.println("Я " + name + " мне " + age + " лет. Мяу!");
+    }
+}
--- instrument/src/Main.java
+++ instrument/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 9; i++) {
-            System.out.println("Hello, world");
+        Cat cat = new Cat("Барсик", 5);
+        for (int i = 0; i < cat.age; i++) {
+            cat.meow();
         }
     }
 }
